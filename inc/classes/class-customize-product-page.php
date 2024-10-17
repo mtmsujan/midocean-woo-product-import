1aff6c66112806db2c34761d42794ae5805647bc
shahjalal <rjshahjalal132@gmail.com>
2024-10-17 14:51:38 +0600

    display conditionally size quantity fields based on type of product

--- inc/classes/class-customize-product-page.php
+++ inc/classes/class-customize-product-page.php
@@ -22,6 +22,7 @@ class Customize_Product_Page {
     private $product_price;
     private $currency_symbol;
     private $technique_labels;
+    private $type_of_product;
 
     public function __construct() {
         $this->setup_hooks();
@@ -91,7 +92,7 @@ class Customize_Product_Page {
             $this->master_code        = $master_code;
             $master_id                = get_post_meta( $product_id, '_master_id', true );
             $country_of_origin        = get_post_meta( $product_id, '_country_of_origin', true );
-            $type_of_products         = get_post_meta( $product_id, '_type_of_products', true );
+            $this->type_of_product    = get_post_meta( $product_id, '_type_of_products', true );
             $commodity_code           = get_post_meta( $product_id, '_commodity_code', true );
             $brand                    = get_post_meta( $product_id, '_brand', true );
             $product_class            = get_post_meta( $product_id, '_product_class', true );
@@ -705,127 +706,140 @@ class Customize_Product_Page {
                             </h3>
                         </div>
 
-                        <!-- Color and Quantity field -->
-                        <div class="product-color-and-quantity-field">
-                            <!-- Color input row configurator. repeater -->
-                            <div class="color-input-container">
-                                <div class="row mt-3 justify-content-between align-items-center color-input-row">
-                                    <!-- Color dropdown -->
-                                    <div class="col-sm-2">
-                                        <div class=" color-dropdown-wrapper">
-                                            <!-- First displayed button -->
-                                            <button class="" type="button" id="colorDropdown" data-bs-toggle="dropdown"
-                                                aria-expanded="false">
-                                                <div class="row align-items-center">
-                                                    <div class="col-4 color-preview">
-                                                        <div style="height: 35px; width: 35px; background: <?php echo strtolower( $this->color_group ); ?>; border: 1px solid black;"
-                                                            class="rounded-circle"></div>
-                                                    </div>
-                                                    <div class="col-8 color-name"><?php echo ucfirst( $this->color_group ); ?> -
-                                                    </div>
-                                                </div>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <!-- /Color dropdown -->
-                                    <!-- Stock input field -->
-                                    <div class="col-sm-6">
-                                        <div class="row flex-column">
-                                            <div class="col-sm-12">
-                                                <div class="input-quantity-wrapper">
-                                                    <input type="number"
-                                                        @keyup="priceCalculationWithoutPrintingCost($el.value, <?php echo $this->product_price; ?>)"
-                                                        placeholder="0" class="input-quantity" x-model="quantity" />
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12 mt-1">
-                                                <div class="row justify-content-around align-items-center">
-                                                    <div class="col-4">
-                                                        <div class="stock-data">
-                                                            <div><span class="stock-value">
-                                                                    <?php echo $this->product_stock; ?></span> En Stock</div>
+                        <?php
+
+                        /**
+                         * Display color and quantity field conditionally
+                         */
+
+                        if ( 'textile' === $this->type_of_product ) : ?>
+
+                            <!-- Color and multiple size, stock field -->
+                            <div class="product-color-size-stock-fields">
+                                <!-- Color input row configurator. repeater -->
+                                <div class="color-input-container">
+                                    <div class="row mt-3 justify-content-between align-items-center color-input-row">
+                                        <!-- Color dropdown -->
+                                        <div class="col-sm-2">
+                                            <div class=" color-dropdown-wrapper">
+                                                <!-- First displayed button -->
+                                                <button class="" type="button" id="colorDropdown" data-bs-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-4 color-preview">
+                                                            <div style="height: 35px; width: 35px; background: <?= strtolower( $this->color_group ); ?>; border: 1px solid black;"
+                                                                class="rounded-circle"></div>
+                                                        </div>
+                                                        <div class="col-8 color-name"><?= ucfirst( $this->color_group ); ?> -
                                                         </div>
                                                     </div>
-                                                    <div class="col-8"></div>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <!-- /Color dropdown -->
+                                        <!-- Stock input field -->
+                                        <div class="col-sm-6">
+                                            <div class="row">
+                                                <!-- Repeat: Single size -->
+                                                <div class="col-3">
+                                                    <div class="d-flex flex-column align-content-center">
+                                                        <div class="text-center size-name">XSS</div>
+                                                        <div class="size-quantity-field">
+                                                            <input class="text-center" type="number" name="" id="">
+                                                        </div>
+                                                        <div class="text-center size-stock-value">
+                                                            <span class="stock-value">
+                                                                120
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- /Repeat: Single size -->
+                                            </div>
+                                        </div>
+                                        <!-- /Stock input field -->
+                                        <!-- Remove color row button -->
+                                        <div class="col-sm-4 remove-color-row">
+                                            <div class="row justify-content-between align-items-center">
+                                                <div class="col-sm-6"></div>
+                                                <div class="col-sm-6 text-end close-button">
+                                                    <!-- <i class="fa-solid fa-xmark"></i> -->
                                                 </div>
                                             </div>
                                         </div>
+                                        <!-- /Remove color row button -->
                                     </div>
-                                    <!-- /Stock input field -->
-                                    <!-- Remove color row button -->
-                                    <div class="col-sm-4 remove-color-row">
-                                        <div class="row justify-content-between align-items-center">
-                                            <div class="col-sm-6"></div>
-                                            <div class="col-sm-6 text-end close-button">
-                                                <!-- <i class="fa-solid fa-xmark"></i> -->
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- /Remove color row button -->
                                 </div>
                             </div>
-                        </div>
-                        <!-- /Color and Quantity field -->
+                            <!-- /Color and multiple size, stock field -->
 
-                        <!-- Color and multiple size, stock field -->
-                        <div class="product-color-size-stock-fields">
-                            <!-- Color input row configurator. repeater -->
-                            <div class="color-input-container">
-                                <div class="row mt-3 justify-content-between align-items-center color-input-row">
-                                    <!-- Color dropdown -->
-                                    <div class="col-sm-2">
-                                        <div class=" color-dropdown-wrapper">
-                                            <!-- First displayed button -->
-                                            <button class="" type="button" id="colorDropdown" data-bs-toggle="dropdown"
-                                                aria-expanded="false">
-                                                <div class="row align-items-center">
-                                                    <div class="col-4 color-preview">
-                                                        <div style="height: 35px; width: 35px; background: <?= strtolower( $this->color_group ); ?>; border: 1px solid black;"
-                                                            class="rounded-circle"></div>
+                        <?php else : ?>
+
+                            <!-- Color and Quantity field -->
+                            <div class="product-color-and-quantity-field">
+                                <!-- Color input row configurator. repeater -->
+                                <div class="color-input-container">
+                                    <div class="row mt-3 justify-content-between align-items-center color-input-row">
+                                        <!-- Color dropdown -->
+                                        <div class="col-sm-2">
+                                            <div class=" color-dropdown-wrapper">
+                                                <!-- First displayed button -->
+                                                <button class="" type="button" id="colorDropdown" data-bs-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-4 color-preview">
+                                                            <div style="height: 35px; width: 35px; background: <?php echo strtolower( $this->color_group ); ?>; border: 1px solid black;"
+                                                                class="rounded-circle"></div>
+                                                        </div>
+                                                        <div class="col-8 color-name"><?php echo ucfirst( $this->color_group ); ?> -
+                                                        </div>
                                                     </div>
-                                                    <div class="col-8 color-name"><?= ucfirst( $this->color_group ); ?> -
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <!-- /Color dropdown -->
+                                        <!-- Stock input field -->
+                                        <div class="col-sm-6">
+                                            <div class="row flex-column">
+                                                <div class="col-sm-12">
+                                                    <div class="input-quantity-wrapper">
+                                                        <input type="number"
+                                                            @keyup="priceCalculationWithoutPrintingCost($el.value, <?php echo $this->product_price; ?>)"
+                                                            placeholder="0" class="input-quantity" x-model="quantity" />
                                                     </div>
                                                 </div>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <!-- /Color dropdown -->
-                                    <!-- Stock input field -->
-                                    <div class="col-sm-6">
-                                        <div class="row">
-                                            <!-- Repeat: Single size -->
-                                            <div class="col-3">
-                                                <div class="d-flex flex-column align-content-center">
-                                                    <div class="text-center size-name">XSS</div>
-                                                    <div class="size-quantity-field">
-                                                        <input class="text-center" type="number" name="" id="">
-                                                    </div>
-                                                    <div class="text-center size-stock-value">
-                                                        <span class="stock-value">
-                                                            120
-                                                        </span>
+                                                <div class="col-sm-12 mt-1">
+                                                    <div class="row justify-content-around align-items-center">
+                                                        <div class="col-4">
+                                                            <div class="stock-data">
+                                                                <div><span class="stock-value">
+                                                                        <?php echo $this->product_stock; ?></span> En Stock</div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-8"></div>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <!-- /Repeat: Single size -->
                                         </div>
-                                    </div>
-                                    <!-- /Stock input field -->
-                                    <!-- Remove color row button -->
-                                    <div class="col-sm-4 remove-color-row">
-                                        <div class="row justify-content-between align-items-center">
-                                            <div class="col-sm-6"></div>
-                                            <div class="col-sm-6 text-end close-button">
-                                                <!-- <i class="fa-solid fa-xmark"></i> -->
+                                        <!-- /Stock input field -->
+                                        <!-- Remove color row button -->
+                                        <div class="col-sm-4 remove-color-row">
+                                            <div class="row justify-content-between align-items-center">
+                                                <div class="col-sm-6"></div>
+                                                <div class="col-sm-6 text-end close-button">
+                                                    <!-- <i class="fa-solid fa-xmark"></i> -->
+                                                </div>
                                             </div>
                                         </div>
+                                        <!-- /Remove color row button -->
                                     </div>
-                                    <!-- /Remove color row button -->
                                 </div>
                             </div>
-                        </div>
-                        <!-- /Color and multiple size, stock field -->
+                            <!-- /Color and Quantity field -->
 
+                        <?php endif;
+
+                        ?>
                         <!-- Add more color button -->
                         <?php
 
