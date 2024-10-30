64c70ce96ca8c6c133b1fd0e0da0f5a918e860dc
shahjalal <rjshahjalal132@gmail.com>
2024-10-30 16:42:15 +0600

    transformed color list name and hex values

--- inc/files/file-transform-color-list.php
+++ inc/files/file-transform-color-list.php
@@ -30,4 +30,29 @@ function transform_color_list() {
     $color_group_result         = json_encode( $color_group_result, JSON_UNESCAPED_SLASHES );
     $color_group_json_file_path = BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/color-group.json';
     file_put_contents( $color_group_json_file_path, $color_group_result );
+
+    // initialize color name result
+    $color_name_values_result = [];
+    // generate color name
+    foreach ( $color_name_values as $value ) {
+
+        $color_id = $value['id'];
+        $hex      = $value['hex'];
+        $name_es  = $value['name']['es_ES'];
+        $group    = $value['group'];
+
+        $color_name_values_result[$color_id] = [
+            'id'    => $color_id,
+            'name'  => $name_es,
+            'hex'   => $hex,
+            'group' => $group,
+        ];
+    }
+
+    // encode to json and save to file
+    $color_name_values_result         = json_encode( $color_name_values_result, JSON_UNESCAPED_SLASHES );
+    $color_name_values_json_file_path = BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/color-name.json';
+    file_put_contents( $color_name_values_json_file_path, $color_name_values_result );
+
+    return "Color list transformed successfully.";
 }
